722694df6712d05b200167611428c3ece3b162c6
Akimov058 <YuAkimov.beeline@yandex.ru>
2024-12-21 00:12:56 +0300

    правки

--- tests.py
+++ tests.py
@@ -1,25 +1,8 @@
 from main import BooksCollector
 import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        #assert len(collector.get_books_rating()) == 2
 
     @pytest.mark.parametrize('book_name',['Самое большое название книги, которое было',''])
     def test_add_new_book_add_invalid_len_name(self,book_name):
@@ -31,7 +14,13 @@ class TestBooksCollector:
     def test_add_new_book(self,book_name):
         collector = BooksCollector()
         collector.add_new_book(book_name)
-        assert collector.get_books_genre().get(book_name)==''
+        assert len(collector.get_books_genre())==1
+
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Му-му')
+        collector.add_new_book('Город N')
+        assert len(collector.get_books_genre())==2
 
     def test_set_book_genre(self):
         collector = BooksCollector()
@@ -47,7 +36,7 @@ class TestBooksCollector:
         genre_name='Военные'
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name,genre_name)
-        assert collector.get_book_genre(book_name)!=genre_name
+        assert len(collector.get_book_genre(book_name))==0
 
     @pytest.mark.parametrize('book_name',['Гарри Поттер часть 1','Гарри Поттер часть 2','Гарри Поттер часть 3'])
     def test_get_books_genre(self,book_name):
@@ -98,6 +87,8 @@ class TestBooksCollector:
         collector.add_book_in_favorites(book_name)
         collector.delete_book_from_favorites(book_name)
         assert len(collector.get_list_of_favorites_books())==0
+
+
 
 
 
